4194c3e2066dc6db44892568efab2cb14685f437
Vans3r <ladno1846@gmail.com>
2025-06-26 10:51:36 +0300

    Ready

--- hm.py
+++ hm.py
@@ -1,14 +1,15 @@
 import os
-from datetime import datetime
 from functools import wraps
+from datetime import datetime
 
 def logger(old_function):
     @wraps(old_function)
     def new_function(*args, **kwargs):
         result = old_function(*args, **kwargs)
-        name_func = old_function.__name__
-        with open('main.log', 'w', encoding='UTF-8') as m:
-            m.write(f'Имя функции: {name_func}. \n Аргументы функции : {args, kwargs}. \n Дата и время выполнения функции : {datetime.now()} \n Результат функции : {result}')
+        name = old_function.__name__
+        with open('main.log', 'a', encoding='utf-8') as m:
+            m.write(f'Название: {name}\nАргументы: {args,kwargs}\nДата: {datetime.now()}\nРезультат: {result}')
+        return result
     return new_function
 
 
@@ -51,3 +52,4 @@ def test_1():
 
 if __name__ == '__main__':
     test_1()
+
